6dcdac58d94f480187028790fc06d7fcdb33f55a
창민 <ckd29672@naver.com>
2023-07-20 10:33:43 +0900

    initialization: git repository 연결

---
+++ src/main/java/com/kimcheese/kimchiservicediscovery/KimchiServiceDiscoveryApplication.java
@@ -0,0 +1,13 @@
+package com.kimcheese.kimchiservicediscovery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KimchiServiceDiscoveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KimchiServiceDiscoveryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kimcheese/kimchiservicediscovery/KimchiServiceDiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kimcheese.kimchiservicediscovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KimchiServiceDiscoveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
